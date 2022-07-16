aef2d55399037d0f0685dfccab8a87cba505a509
AbubakarA-Dot <abubakarbryar@gmail.com>
2022-07-16 13:15:39 +0500

    Converted FBV to CBV for search feature

--- search/queryObj/urls.py
+++ search/queryObj/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ssearch, name='search'),
     path('reports_list', views.reports_list, name='reports_list'),
-    path('search_by_q_objects', views.search_by_q_objects, name="search_by_q_objects"),
+    # path('search_by_q_objects', views.search_by_q_objects, name="search_by_q_objects"),
+    path('search_by_q_objects', views.RecommendationsChecksRanSearch.as_view(), name="search_by_q_objects")
 ]
